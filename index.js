9182d44785166303e91fc7f5edad62d4a9286dce
FouzanPS <mohammedfouzanmsd7@gmail.com>
2024-11-06 02:51:54 +0530

    Payment method added

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import express from "express";
-import paypal from './services/paypal.js'
+import { generateAccessToken, createOrder, capturePayment } from "./services/paypal.js";
 
 const app = express();
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send("Welcome to my server!")
+    res.send("Home Page")
+})
+
+app.post('/pay', async (req, res) => {
+    try {
+        const url = await createOrder(); // Await the async function
+        res.send(`<a href="${url}">Click here to complete the payment</a>`); // Send a clickable link
+    } catch (err) {
+        res.send('Error: ' + err);
+    }
+});
+
+
+app.get('/complete-order', async (req,res) => {
+    try{
+        capturePayment(req.query.token)
+        res.send("Order Placed!")
+    }
+    catch(err){
+        res.send('Error: ' + err)
+    }
+})
+
+app.get('/cancel-order', (req,res) => {
+    res.send("Order Cancelled!")
 })
 
 app.listen(PORT,() => {
--- services/paypal.js
+++ services/paypal.js
@@ -13,7 +13,70 @@ async function generateAccessToken(){
             password: process.env.PAYPAL_SECRET
         }
     })
-    console.log(response.data)
+    return response.data.access_token
 }
 
-export default generateAccessToken()
+async function createOrder(){
+    const accessToken = await generateAccessToken();
+
+    const response = await axios({
+        url: process.env.PAYPAL_URL + '/v2/checkout/orders',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        data: JSON.stringify({
+            "intent": "CAPTURE",
+            purchase_units: [{
+                items: [{
+                    name: "Product Name",
+                    description: "Product Desc",
+                    quantity: 1,
+                    unit_amount: {
+                        currency_code: 'USD',
+                        value: '100.0'
+                    }
+                }],
+                amount: {
+                    currency_code: 'USD',
+                    value: '100.0',
+                    breakdown:{
+                        item_total: {
+                            currency_code: 'USD',
+                            value: '100.0'
+                        }
+                    }
+                }
+            }],
+            application_context: {
+                return_url:  process.env.BASE_URL + '/complete-order',
+                cancel_url:  process.env.BASE_URL + '/cancel-order',
+                user_action: 'PAY_NOW'
+            }
+        })
+    })
+    const approveLink = response.data.links.find(link => link.rel == 'approve').href
+    console.log(approveLink)
+    return approveLink
+}
+
+async function capturePayment(orderId){
+    const accessToken = await generateAccessToken()
+
+    const response = await axios({
+        url: process.env.PAYPAL_URL + `/v2/checkout/orders/${orderId}/capture`,
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+
+    })
+    return response.data
+}
+
+createOrder().catch(error => {
+    console.error('Error creating order:', error);})
+
+export {generateAccessToken, createOrder, capturePayment}
